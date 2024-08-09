8fffd1973058b52192f7e80833ab5b3e80eb183c
pakpoom.s@transcodeglobal.com <pakpoom.s@transcodeglobal.com>
2024-08-09 16:15:38 +0700

    udpate

--- myapp.js
+++ myapp.js
@@ -1,11 +1,11 @@
 const express = require('express'); //เรียกใช้ express ผ่าน require
 const { swaggerUi, swaggerDocs } = require('./swagger'); // Import Swagger configuration
 const app = express(); //สร้างตัวแปร myApp เพื่อใช้งาน express 
-const port = 3000; //พอร์ตของ Server ที่ใช้ในการเปิด Localhost 
+const port = 8000; //พอร์ตของ Server ที่ใช้ในการเปิด Localhost 
 const client_id = 'myclient';
 const client_secret = 'pclEGbe5Q4FHqEMeTcNPqq3F2WhQG068';
-//const REACT_APP_URL = 'http://141.11.33.31:8080';
-const REACT_APP_URL = 'http://localhost:8080';
+const REACT_APP_URL = 'http://141.11.33.31:8080';
+//const REACT_APP_URL = 'http://localhost:8080';
 const cors = require('cors');
 const bodyParser = require('body-parser'); // Import body-parser
 
